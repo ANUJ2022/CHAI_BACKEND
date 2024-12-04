badedb36428c4565fe123395bacbea6def45e4fa
Anuj Negi <anujnegi234@gmail.com>
2024-12-04 20:24:23 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@
 
 import dotenv from 'dotenv';
 import connectDB from './db/dbConnect.js';
+import { app } from './app.js';
 
 
 dotenv.config({
     path: './env'
 })
 
+app.on("error", (error) => {
+    console.log("ERR:", error);
+    throw error; // Optional: rethrow the error if needed
+});
 
 
-
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(` Server is running at port : ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB connection failed !!!", err);
+    })
 
 
 
